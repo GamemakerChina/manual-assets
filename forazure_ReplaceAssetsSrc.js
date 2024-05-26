653677593e9b1c8be9e6dda01378ed5374f48298
LiarOnce <liaronce@hotmail.com>
2024-05-26 23:21:38 +0800

    upload

---
+++ forazure_ReplaceAssetsSrc.js
@@ -0,0 +1,35 @@
+"use strict";
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
+
+let assetsOutsideUrl = "https://manual-assets.gm-cn.top"
+
+glob('**/*.htm', {}, (err, files) => {
+    if (err) {
+        console.log("错误：" + err)
+    } else {
+        for (let index = 0; index < files.length; index++) {
+            let normalizeName = path.normalize(files[index])
+            let fileHTML = fs.readFileSync(files[index]).toString()
+            // console.log(json)
+            let assetsImagesLocation = 'src="' + assetsOutsideUrl + 'assets/Images'
+            let assetsVideosLocation = 'src="' + assetsOutsideUrl + 'assets/Videos'
+            
+            let final = fileHTML.replace('src="assets/Images', assetsImagesLocation)
+                                .replace('src="../assets/Images', assetsImagesLocation)
+                                .replace('src="../../assets/Images', assetsImagesLocation)
+                                .replace('src="../../../assets/Images', assetsImagesLocation)
+                                .replace('src="../../../../assets/Images', assetsImagesLocation)
+                                .replace('src="../../../../../assets/Images', assetsImagesLocation)
+                                .replace('src="../../assets/Videos', assetsVideosLocation)
+                                .replace('src="../../../../../assets/Videos', assetsVideosLocation)
+            
+            fs.writeFile(normalizeName, final, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        }
+    }
+})
