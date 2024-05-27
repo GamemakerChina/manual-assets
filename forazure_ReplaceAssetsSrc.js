fdb4cf2d2e175983c2c515918df0f29d3a3b967e
Mark Shi <qq1062787843@hotmail.com>
2024-05-27 11:40:28 +0800

    Update forazure_ReplaceAssetsSrc.js

--- forazure_ReplaceAssetsSrc.js
+++ forazure_ReplaceAssetsSrc.js
@@ -6,6 +6,10 @@ const fs = require("fs");
 let assetsOutsideUrl = "https://manual-assets.gm-cn.top"
 let export_directory = "../"
 
+let default_css = fs.readFileSync(export_directory + "assets/css/default.css").toString()
+let final_css = default_css.replaceAll('../Images/Icons', assetsOutsideUrl + '/Images/Icons')
+fs.writeFileSync(path.normalize(export_directory + "assets/css/default.css"), final_css)
+
 glob(export_directory + '**/*.htm', {}, (err, files) => {
     if (err) {
         console.log("错误：" + err)
